e1a89a12ce1745fc6fb0029d3f82ca0b3de6b2de
Dennis Laumen <dennis.laumen@ihomer.nl>
2014-04-26 20:18:20 +0200

    Third revision of leap in Go
    I still wasn't content with the overly complex `if` structure. I started
    this rewrite trying to write it using the Go's flexible `switch`
    statement but while rewriting I decided to use predicates to make it a
    bit more readable (even though it's really verbose for a problem this
    simple).
    
    See the exercism.io submission for context:
    http://exercism.io/submissions/b4a2d763c0872b381c37337e

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -6,13 +6,20 @@ package leap
 // - except every year that is evenly divisible by 100
 // - unless the year is also evenly divisible by 400
 func IsLeapYear(year int) bool {
-	if year%4 != 0 {
-		return false
-	} else if year%100 != 0 {
-		return true
-	} else if year%400 == 0 {
-		return true
-	} else {
-		return false
-	}
+	return isFourthYear(year) && (!isFirstYearOfCentury(year) || isFirstYearOfFourthCentury(year))
+}
+
+// isFourthYear reports whether the given year is a fourth year (i.e. divisible by four).
+func isFourthYear(year int) bool {
+	return year%4 == 0
+}
+
+// isFirstYearOfCentury reports whether the given year is the first year of a century (in astronomical year numbering).
+func isFirstYearOfCentury(year int) bool {
+	return year%100 == 0
+}
+
+// isFirstYearOfFourthCentury reports whether the given year is the first year of a fourth century (in astronomical year numbering).
+func isFirstYearOfFourthCentury(year int) bool {
+	return year%400 == 0
 }
